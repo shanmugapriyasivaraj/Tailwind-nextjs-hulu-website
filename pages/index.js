4096df49026537dff2398bc906a756c2f94721b1
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-27 13:54:57 +0530

    Api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       </Head>
       {/* header */}
       <Header />
+
+      <Nav />
     </div>
   );
 }
